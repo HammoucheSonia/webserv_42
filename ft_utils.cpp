861637c7f91b4919db1422d76c483766ec322001
Sonia HAMMOUCHE <shammouc@e2r2p1.clusters.42paris.fr>
2023-05-30 18:42:25 +0200

    ajout split

--- ft_utils.cpp
+++ ft_utils.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 #include <cstring>
 
+std::vector<std::string> split(const std::string& str, char delimiter) {
+    std::vector<std::string> tokens;
+    std::string token;
+    size_t start = 0;
+    size_t end = str.find(delimiter, start);
+
+    while (end != std::string::npos) {
+        token = str.substr(start, end - start);
+        tokens.push_back(token);
+        start = end + 1;
+        end = str.find(delimiter, start);
+    }
+
+    token = str.substr(start);
+    tokens.push_back(token);
+
+    return tokens;
+}
+
 bool isValidHTTPMethod(const char* method) {
     if (strcmp(method, "GET") == 0 ||
         strcmp(method, "POST") == 0 ||
@@ -27,7 +46,6 @@ const char *find_methode(std :: string request)
 
 const char *find_uri(std :: string request)
 {
-    request = request.substr(0, request.find('\r'));
     int i = request.find(' ');
     int j = request.find(' ', i + 1);
     std::string uri = request.substr(i + 1, j - i - 1);
